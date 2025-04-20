b5993a68b1d86cc973fa8195f8da070db781bbc2
hc5474 <hc5474@g.rit.edu>
2025-04-19 22:20:18 -0400

    .

--- dector_v2/detector_v2.py
+++ dector_v2/detector_v2.py
@@ -520,8 +520,8 @@ if __name__ == "__main__":
             monitor_logic.run_monitoring(
                 iface=monitor_iface_active,
                 config_dict=config,
-                baseline_profiles=baseline_profiles,
-                known_bssids=known_bssids
+                baseline_profiles_dict=baseline_profiles,
+                known_bssids_dict=known_bssids
             )
 
     except Exception as e:
