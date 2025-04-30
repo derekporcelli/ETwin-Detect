04b186a240fa8bbb18f55c8c3dedec28ab5eec20
hc5474 <hc5474@g.rit.edu>
2025-04-30 02:48:51 -0400

    br

--- detector_v3/detector_v3.py
+++ detector_v3/detector_v3.py
@@ -14,7 +14,7 @@ import sys
 import time
 import pandas as pd
 from collections import defaultdict
-import dector_v3.monitor_logic as monitor_logic
+import det.monitor_logic as monitor_logic
 import scapy.all as scapy
 from scapy.layers.dot11 import Dot11, Dot11Beacon, Dot11ProbeResp
 
